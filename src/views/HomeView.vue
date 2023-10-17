f215743bacfaf0afce70357e0499482902d8abcd
MrSkulason <hjorturmani@gmail.com>
2023-10-17 13:43:46 +0000

    Update HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,25 +6,34 @@ export default {
     return {
       matter: [5, 0],
       interval: null,
-      multiplier1: 1,
-      baseAmount1: 0,
-      amount1: 0,
-      persec: 0
+      //baseB[0] = base amount, baseB[1] = total baseBs, baseB[2] = powerOf, baseB[3] multiplier, baseB[4] powerOf
+      baseB: [0, 0, 0, 1, 0],
+
+      perSec: [0, 0]
     }
   },
   methods: {
     increase1: function () {
-      this.amount1++;
-      this.baseAmount1++;
-      if (this.baseAmount1%10 == 0){
-        this.multiplier1=this.multiplier1*2;
+      this.baseB[1]++;
+      this.baseB[0]++;
+      if (this.baseB[0]%10 == 0){
+        this.baseB[3]=this.baseB[3]*2;
       }
     },
+    perSecond(){
+      this.perSec[0]=this.baseB[1]*this.baseB[3]
+      this.perSec[1]=this.baseB[2]+this.baseB[4]
+      console.log(this.perSec)
+      return this.powerOf(this.perSec[0], this.perSec[1])
+    },
     increase(number, number2) {
-      if (number[1]-number2[1] > 10){this.powerOf(number[0], number[1])}
-      else if (number[1]-number2[1] < -10){this.powerOf(number2[0], number[1])}
-      number[0] = number[0] + (number2[0]*(10^(number2[1]-number[1])))
-      return this.powerOf(number[0], number[1])
+      console.log(number)
+      console.log(number2)
+      if (number[1]-number2[1] > 10){return [number[0], number[1]]}
+      else if (number[1]-number2[1] < -10){[number2[0], number2[1]]}
+      else {number[0] = number[0] + (number2[0]/(10^(number[1]-number2[1])))}
+
+        return this.powerOf(number[0], number[1])
       },
 
     stopInterval() {
@@ -34,15 +43,16 @@ export default {
       if(mainNumber > 10 || mainNumber < 1) {
         let TruePowerOf = Math.floor(Math.log10(mainNumber))
         powerOf += TruePowerOf
-
+        console.log(mainNumber)
         mainNumber = mainNumber/(10^TruePowerOf)
       }
+      mainNumber = Math.ceil((mainNumber*10000))/10000
       return [mainNumber, powerOf]
     }
   },
   mounted() {
-    this.interval = setInterval(() => {this.persec = this.amount1 * this.multiplier1}, 1000)
-    this.interval = setInterval(() => {this.matter[0] = this.matter[0] + this.persec}, 1000)
+    this.interval = setInterval(() => {this.persec = this.perSecond()}, 100)
+    this.interval = setInterval(() => {this.matter = this.increase(this.matter, this.persec) }, 100)
   }
 }
 
@@ -51,7 +61,7 @@ export default {
 <template>
   <p class="text-white">{{matter[0]}}e{{matter[1]}}</p>
 
-  <button class="bg-white rounded text-black p-4" @click="increase1">Inner Singularity {{amount1}} {{multiplier1}}</button>
+  <button class="bg-white rounded text-black p-4" @click="increase1">Inner Singularity {{baseB[1]}}e{{baseB[2]}} {{baseB[3]}}e{{baseB[4]}}</button>
 
 
 
