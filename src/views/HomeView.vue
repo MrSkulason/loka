d072967359e665db7adfc77cf146d08c6d790685
MrSkulason <hjorturmani@gmail.com>
2023-11-02 09:11:48 +0000

    Update HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,14 +4,14 @@
 export default {
   data() {
     return {
-      matter: [5, 0],
+      matter: [0, [5, 0], [0]],
       interval: null,
       //baseB[0] = base amount, baseB[1] = total baseBs, baseB[2] = powerOf, baseB[3] multiplier, baseB[4] powerOf
       baseB: [0, [0, 0], [1, 0]],
       secondB: [0, [0, 0], [1, 0]],
       prettyNumber: [],
       //list in list to increase
-      perSec: [[],[],[]]
+      perSec: [[0, 0],[0, 0],[0, 0]]
     }
   },
   methods: {
@@ -32,18 +32,17 @@ export default {
       let perSec = [0, 0]
       perSec[0]= number[1][0]*number[2][0]
       perSec[1]=number[1][1]+number[2][1]
+      console.log(number)
       console.log(perSec)
       return this.powerOf(perSec[0], perSec[1])
     },
     increase(number, number2) {
-      console.log(number)
-      console.log(number2)
-      console.log(((number[1]-number2[1])))
-      if (number[1]-number2[1] > 10){return [number[0], number[1]]}
-      else if (number[1]-number2[1] < -10){return [number2[0], number2[1]]}
-      else {number[0] = number[0] + (number2[0]*(Math.pow(10,number2[1]-number[1])))}
 
-        return this.powerOf(number[0], number[1])
+      if (number[1][1]-number2[1] > 10){return [number[1][0], number[1][1]]}
+      else if (number[1][1]-number2[1] < -10){return [number2[1][0], number2[1]]}
+      else {number[1][0] = number[1][0] + (number2[0]*(Math.pow(10,number2[1]-number[1][1])))}
+
+        return this.powerOf(number[1][0], number[1][1])
       },
 
     stopInterval() {
@@ -72,16 +71,18 @@ export default {
     this.interval = setInterval(() => {
       //mass
       this.perSec[0] = this.perSecond(this.baseB),
-      this.matter = this.increase(this.matter[0], this.perSec[0]),
+      this.matter[1] = this.increase(this.matter, this.perSec[0])
+      console.log(this.perSec)
       //increases BaseB
       this.perSec[1] = this.perSecond(this.secondB),
-      this.matter = this.increase(this.baseB[1], this.perSec[1])
+      this.baseB[1] = this.increase(this.baseB, this.perSec[1])
     }, 100)
     this.interval = setInterval(() => {
-      this.prettyNumber[0] = this.prettyNumb(this.matter[0]),
+      this.prettyNumber[0] = this.prettyNumb(this.matter[1][0]),
       this.prettyNumber[1] = this.prettyNumb(this.baseB[1][0]),
       this.prettyNumber[2] = this.prettyNumb(this.baseB[2][0]),
-      this.prettyNumber[3] = this.prettyNumb(this.baseB[0])
+      this.prettyNumber[3] = this.prettyNumb(this.secondB[1][0])
+      this.prettyNumber[4] = this.prettyNumb(this.secondB[2][0])
     }, 100)
 
   }
@@ -90,9 +91,10 @@ export default {
 </script>
 
 <template>
-  <p class="text-white">{{prettyNumber[0]}}e{{matter[1]}}</p>
+  <p class="text-white">{{prettyNumber[0]}}e{{matter[1][1]}}</p>
 
   <button class="bg-white rounded text-black p-4" @click="increase1(this.baseB)">Inner Singularity {{prettyNumber[1]}}e{{baseB[1][1]}} {{prettyNumber[2]}}e{{baseB[2][1]}}</button>
+  <button class="bg-white rounded text-black p-4" @click="increase1(this.secondB)">Second {{prettyNumber[3]}}e{{secondB[1][1]}} {{prettyNumber[4]}}e{{secondB[2][1]}}</button>
 
 
 
